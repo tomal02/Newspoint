b23c3ee976ce50634b8ed0281613715c3d87d6d4
Tom Lee <tomalee2002@gmail.com>
2021-10-11 00:47:01 +0100

    adds describe to spec constructor

--- spec/helpers/constructers.js
+++ spec/helpers/constructers.js
@@ -1,3 +1,10 @@
 const expect = (input) => {
   return new Matchers(input);
 };
+
+const describe = (label, callback) => {
+  console.log(`----------Describe:  ${label}----------`);
+  console.log(`---------------------------------------`);
+  console.log('');
+  callback();
+};

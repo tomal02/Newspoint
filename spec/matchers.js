f9f367eeca6c8e6cc2cb455f5492d7aa0dda2a38
Tom Lee <tomalee2002@gmail.com>
2021-10-10 23:19:12 +0100

    adds toNotEqual() to Matchers class

--- spec/matchers.js
+++ spec/matchers.js
@@ -15,4 +15,12 @@ class Matchers {
       : `FAIL: Input: ${this.input} is NOT EQUAL to ${matchValue}`;
     this.printResult(message, result);
   }
+
+  toNotEqual(matchValue) {
+    let result = this.input !== matchValue;
+    let message = result
+      ? `PASS: Input: ${this.input} is NOT EQUAL to ${matchValue}`
+      : `FAIL: Input: ${this.input} is EQUAL to ${matchValue}`;
+    this.printResult(message, result);
+  }
 }

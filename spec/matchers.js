b246182186f48d99a170b05a625b90f006617810
Tom Lee <tomalee2002@gmail.com>
2021-10-10 23:11:14 +0100

    adds printResult function to Matchers class

--- spec/matchers.js
+++ spec/matchers.js
@@ -2,4 +2,9 @@ class Matchers {
   constructor(input) {
     this.input = input;
   }
+
+  printResult(message, result) {
+    let css = result ? 'green' : 'red';
+    console.log(`%c ${message}`, `background: ${css};`);
+  }
 }

55c63073918a55ce6d489dae3f262a9f64ee80eb
Tom Lee <tomalee2002@gmail.com>
2021-10-11 02:56:40 +0100

    adds fetching articles function to interface

--- src/interface.js
+++ src/interface.js
@@ -1,1 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  let client = new Client();
+
+  function fetchArticles() {
+    client
+      .getArticles()
+      .then((results) => {
+        console.log(results);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  fetchArticles();
+});

b13a49f1f77328646c3bd6ba662a4f96bfe1da47
Tom Lee <tomalee2002@gmail.com>
2021-10-11 01:49:34 +0100

    fixed load error with running tests

--- spec/storiesSpec.js
+++ spec/storiesSpec.js
@@ -3,14 +3,14 @@ describe('Stories', () => {
 
   it('can return all stories', () => {
     news.stories = ['testStory1', 'testStory2'];
-    expect(stories.getAll()).toEqual(['testStory1', 'testStory2']);
+    expect(news.getAll()).toEqual(['testStory1', 'testStory2']);
   });
 
   it('can store story attributes correctly', () => {
     let testStory1 = new Story('title1', 'body1', 'image1', 'source1');
     let testStory2 = new Story('title2', 'body2', 'image2', 'source2');
     news.stories = ['testStory1', 'testStory2'];
-    let result = stories.getAll();
+    let result = news.getAll();
     expect(result[0].title).toEqual('title1');
     expect(result[0].body).toEqual('title1');
     expect(result[1].source).toEqual('title1');

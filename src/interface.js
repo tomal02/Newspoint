82fd19aab9002a46feea60379511249fbc9fd61c
Tom Lee <tomalee2002@gmail.com>
2021-10-12 02:32:01 +0100

    adds fetchStories to DOM

--- src/interface.js
+++ src/interface.js
@@ -1,1 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  const stories = new Stories();
+
+  const fetchStories = () => {
+    fetch('https://enhkjf1ko7k3jgo.m.pipedream.net')
+      .then((response) => response.json())
+      .then((data) => {
+        data.storys.forEach((story) => {
+          stories.addStory(
+            new Story({
+              title: story.webTitle,
+              body: story.fields.bodyText,
+              image: story.fields.thumbnail,
+              source: story.webUrl,
+            })
+          );
+        });
+      });
+  };
+  fetchStories();
+});

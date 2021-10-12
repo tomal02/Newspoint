1442484bf8b0b414285f700c30e39c18f38a50c7
Tom Lee <tomalee2002@gmail.com>
2021-10-12 02:45:22 +0100

    updates spec test for Stories

--- spec/storiesSpec.js
+++ spec/storiesSpec.js
@@ -3,6 +3,6 @@ describe('Stories', () => {
 
   it('can return all stories', () => {
     news.stories = ['testStory1', 'testStory2'];
-    expect(news.getAll()).toEqual(['testStory1', 'testStory2']);
+    expect(news.getAll().length).toEqual(2);
   });
 });
